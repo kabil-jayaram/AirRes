e0926d9be392668e304eecbbef6e8e285d243b34
Kabilan J <kabil.jayaram@gmail.com>
2024-02-19 18:55:07 +0530

    Update DataManager.java

--- DataManager.java
+++ DataManager.java
@@ -1,51 +1,36 @@
 import java.io.*;
 import java.util.*;
 
-enum AccountType {
-    CUSTOMER, STAFF, ADMIN
-}
-
-class AccountIdData implements Serializable {
-    private String id;
-    private AccountType type;
-
-    public AccountIdData(String id, AccountType type) {
-        this.id = id;
-        this.type = type;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public AccountType getType() {
-        return type;
-    }
-}
-
+// Main class for managing account data
 public class DataManager {
+    // Map to store account data of different types
     private static Map<Class<?>, List<AccountIdData>> accountIdsMap = new HashMap<>();
 
+    // Method to add data to the map
     public static void addData(Class<?> accountType, AccountIdData data) {
         List<AccountIdData> accountIds = accountIdsMap.getOrDefault(accountType, new ArrayList<>());
         accountIds.add(data);
         accountIdsMap.put(accountType, accountIds);
     }
 
+    // Method to get data from the map
     public static List<AccountIdData> getData(Class<?> accountType) {
         return accountIdsMap.getOrDefault(accountType, new ArrayList<>());
     }
 
+    // Method to save data to a file
     public static void saveData(Class<?> accountType, AccountType type) {
         List<AccountIdData> accountIds = accountIdsMap.getOrDefault(accountType, new ArrayList<>());
         saveAccountIds(accountIds, type);
     }
 
+    // Method to load data from a file
     public static void loadData(Class<?> accountType, AccountType type) {
         List<AccountIdData> accountIds = loadAccountIds(type);
         accountIdsMap.put(accountType, accountIds);
     }
 
+    // Method to save account IDs to a file
     private static void saveAccountIds(List<AccountIdData> accountIds, AccountType type) {
         String filename = type.toString().toLowerCase() + "_ids.dat";
         try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filename))) {
@@ -55,6 +40,7 @@ public class DataManager {
         }
     }
 
+    // Method to load account IDs from a file
     private static List<AccountIdData> loadAccountIds(AccountType type) {
         String filename = type.toString().toLowerCase() + "_ids.dat";
         try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filename))) {
